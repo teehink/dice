7a58ef7aafc8d13cebf466dc9de106ae472e97bf
tristan <teehink@gmail.com>
2021-07-03 14:29:00 -1000

    die roll on button click added

--- scripty.js
+++ scripty.js
@@ -1,4 +1,16 @@
 let player1Score = 0
 let player2Score = 0
 let player1Turn = true;
-let pips = 0
+
+const rollBtn = document.getElementById('rollBtn')
+const resetBtn = document.getElementById('resetBtn')
+const message = document.getElementById('message')
+const player1Scoreboard = document.getElementById('player1Scoreboard')
+const player2Scoreboard = document.getElementById('player2Scoreboard')
+const player1Die = document.getElementById('player1Die')
+const player2Die = document.getElementById('player2Die')
+
+rollBtn.addEventListener('click', function() {
+  const dieRoll = Math.floor(Math.random() * 6) + 1
+  console.log(dieRoll)
+})
